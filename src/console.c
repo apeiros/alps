868de501f9d4df4eb07e1af3545c6277035090da
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-12-07 21:39:21 +0100

    Tried multiple ticks on a drop.

--- src/console.c
+++ src/console.c
@@ -4,6 +4,7 @@
 int main(int argc, char * argv[]) {
   AlpsVector pos, vel;
   AlpsDrop drop;
+  int i;
 
   pos   = alpsvector(165.0, -1.0);
   vel   = alpsvector(1.2, 4.3);
@@ -11,9 +12,12 @@ int main(int argc, char * argv[]) {
 
   alpsdrop_print(drop);
   printf("\n");
-  alpsdrop_tick(&drop);
-  alpsdrop_print(drop);
-  printf("\n");
+
+  for(i=0; i<10; i++) {
+    alpsdrop_tick(&drop);
+    alpsdrop_print(drop);
+    printf("\n");
+  }
 
   return 0;
 }
